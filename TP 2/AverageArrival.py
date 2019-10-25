b279453ebfbc18330c64ba7a01e2401a48ebe9ab
Gayel Abou Imad <gayelabouimad@outlook.com>
2019-10-25 22:32:44 +0300

    code added

---
+++ TP 2/AverageArrival.py
@@ -0,0 +1,25 @@
+import csv
+
+liste_arrivee = []
+
+with open('log_sf7_5s_100.csv', mode='r') as logs:
+    csv_reader = csv.reader(logs, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        var = (row[0])[7:]
+        sender_number = var.split(":")[0]
+        liste_arrivee.append(sender_number)
+        line_count +=1
+    print(line_count)
+    # print(liste_arrivee)
+    
+dictio = {}
+for sender in liste_arrivee:
+    if sender in dictio:
+        dictio[str(sender)] += 1
+    else:
+        dictio[str(sender)] = 1
+
+print(dictio)
+
+print(len(dictio))
