2ba6d172df0765ad6330393e5677507299c1f0e8
Gayel Abou Imad <gayelabouimad@outlook.com>
2019-10-25 22:56:06 +0300

    code refactored

--- TP 2/AverageArrival.py
+++ TP 2/AverageArrival.py
@@ -1,38 +1,44 @@
 import csv
 
-liste_arrivee = []
+def getData(file_name):
+    with open('log_sf7_5s_100.csv', mode='r') as logs:
+        liste_arrivee = []
+        csv_reader = csv.reader(logs, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            var = (row[0])[7:]
+            sender_number = var.split(":")[0]
+            liste_arrivee.append(sender_number)
+            line_count +=1
+    return liste_arrivee
 
-with open('log_sf7_5s_100.csv', mode='r') as logs:
-    csv_reader = csv.reader(logs, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        var = (row[0])[7:]
-        sender_number = var.split(":")[0]
-        liste_arrivee.append(sender_number)
-        line_count +=1
-    print(line_count)
-    # print(liste_arrivee)
-    
-dictio = {}
-for sender in liste_arrivee:
-    if sender in dictio:
-        dictio[str(sender)] += 1
-    else:
-        dictio[str(sender)] = 1
+def generateFirstDict(data):
+    dictio = {}
+    for sender in data:
+        if sender in dictio:
+            dictio[str(sender)] += 1
+        else:
+            dictio[str(sender)] = 1
+    return dictio
 
-dictio_average = {}
+def generateSecondDict(firstDict):
+    dictio_average = {}
+    for key in firstDict.keys():
+        dictio_average[key] = firstDict[key]/100
 
-for key in dictio.keys():
-    dictio_average[key] = dictio[key]/100
+    return dictio_average
 
-print(dictio_average)
+def generateThirdDict(secondDict):
+    somme = 0
+    for value in secondDict.values():
+        somme = somme + float(value)
+    return somme/21
 
-dictio_all_average = {}
+def main():
+    data = getData('log_sf7_5s_100.csv')
+    nb_msg_recu_par_pers = generateFirstDict(data)
+    avg_msg_par_pers = generateSecondDict(nb_msg_recu_par_pers)
+    average = generateThirdDict(avg_msg_par_pers)
+    print (average*100, "% des messages ont ete recus")
 
-somme = 0
-for value in dictio_average.values():
-    somme = somme + float(value)
-
-average = somme/21
-
-print (average*100, "% des messages ont ete recus")
+main()
