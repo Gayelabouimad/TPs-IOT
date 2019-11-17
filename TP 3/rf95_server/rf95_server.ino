c4b869bd2ebdc351d40fe175f24dccfd31c98199
Gayel Abou Imad <gayelabouimad@outlook.com>
2019-11-17 11:31:13 +0200

    server added

---
+++ TP 3/rf95_server/rf95_server.ino
@@ -0,0 +1,58 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+
+RH_RF95 rf95;
+
+
+int led = 8;
+
+float frequency = 868.3;
+
+void setup() 
+{
+ 
+  Serial.begin(115200);
+  while (!Serial) ; // Wait for serial port to be available
+  if (!rf95.init())
+    Serial.println("init failed");  
+
+  rf95.setFrequency(frequency);
+  // Setup Power,dBm
+  rf95.setTxPower(13);
+
+  // Setup Spreading Factor (6 ~ 12)
+  rf95.setSpreadingFactor(7);
+  
+  // Setup BandWidth, option: 7800,10400,15600,20800,31250,41700,62500,125000,250000,500000
+  //Lower BandWidth for longer distance.
+  rf95.setSignalBandwidth(125000);
+  
+  // Setup Coding Rate:5(4/5),6(4/6),7(4/7),8(4/8) 
+  rf95.setCodingRate4(5);
+
+}
+
+void loop()
+{
+  if (rf95.available())
+  {
+    // Do not overload this part, should mainly be waiting for new messages
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.recv(buf, &len))
+    {
+      Serial.print("Got message: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+
+
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+}
